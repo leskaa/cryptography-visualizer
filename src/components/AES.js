cf974aa4c44ce3b4894f1fe34ad34869ad021109
Mitchell Knox <mitchk612@yahoo.com>
2020-11-07 14:37:44 -0600

    initialized AES componenet interaction

--- src/components/AES.js
+++ src/components/AES.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { Typography } from 'antd';
+import React, { Component } from 'react';
+import AES_MATRIX from './AES_Matrix';
 
-const { Title } = Typography;
-
-const AES = () => {
-  return <Title>AES</Title>;
+const data = {
+  encrypt: 'abcdefghi'
 };
+
+class AES extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="AES">
+        <AES_MATRIX {...data}></AES_MATRIX>
+      </div>
+    );
+  }
+}
 
 export default AES;
---
+++ src/components/AES_Matrix.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AES_Matrix extends Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.encrypt}</h1>
+            </div>
+        )
+    }
+}
+
+export default AES_Matrix;
