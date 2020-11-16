8998dc8059c806e319cf7e43729eb65b0c3c1f2d
Mitchell Knox <mitchk612@yahoo.com>
2020-11-15 21:02:23 -0600

    initialized basic matrix structure

--- src/components/AES.js
+++ src/components/AES.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import AES_MATRIX from './AES_Matrix';
 
-const data = {
-  encrypt: 'abcdefghi'
-};
-
 class AES extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      col1: ['A', 'B', 'C', 'D'],
+      col2: ['A', 'B', 'C', 'D'],
+      col3: ['A', 'B', 'C', 'D'],
+      col4: ['A', 'B', 'C', 'D']
+    };
+  }
+
+  setEncryptText(value) {
+    this.setState({
+      encrypt: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M']
+    });
   }
 
   render() {
     return (
       <div className="AES">
-        <AES_MATRIX {...data}></AES_MATRIX>
+        <AES_MATRIX value={this.state}></AES_MATRIX>
       </div>
     );
   }
--- src/components/AES_Matrix.js
+++ src/components/AES_Matrix.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react';
+import { Table } from 'antd';
 
 class AES_Matrix extends Component {
+
+  dataSource = [
+    {
+      key: 'r1',
+      c1: this.props.value.col1[0],
+      c2: this.props.value.col2[0],
+      c3: this.props.value.col3[0],
+      c4: this.props.value.col4[0],
+    },
+    {
+      key: 'r2',
+      c1: this.props.value.col1[1],
+      c2: this.props.value.col2[1],
+      c3: this.props.value.col3[1],
+      c4: this.props.value.col4[1],
+    },
+    {
+      key: 'r3',
+      c1: this.props.value.col1[2],
+      c2: this.props.value.col2[2],
+      c3: this.props.value.col3[2],
+      c4: this.props.value.col4[2],
+    },
+    {
+      key: 'r4',
+      c1: this.props.value.col1[3],
+      c2: this.props.value.col2[3],
+      c3: this.props.value.col3[3],
+      c4: this.props.value.col4[3],
+    },
+  ];
+  
+  columns = [
+    {
+      dataIndex: 'c1',
+    },
+    {
+      dataIndex: 'c2',
+    },
+    {
+      dataIndex: 'c3',
+    },
+    {
+      dataIndex: 'c4',
+    },
+  ];
+
     render() {
         return (
             <div>
-                <h1>{this.props.encrypt}</h1>
+                <Table dataSource={this.dataSource} columns={this.columns} />
             </div>
         )
     }
