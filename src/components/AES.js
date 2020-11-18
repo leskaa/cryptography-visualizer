a072d56e8600bb8f47554b2f318e3d8799fed234
Mitchell Knox <mitchk612@yahoo.com>
2020-11-17 20:15:51 -0600

    added string to byte array conversion

--- src/components/AES.js
+++ src/components/AES.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import AES_MATRIX from './AES_Matrix';
 
+var sizeof = require('object-sizeof');
+
 class AES extends Component {
   constructor(props) {
     super(props);
@@ -10,6 +12,7 @@ class AES extends Component {
       col3: ['A', 'B', 'C', 'D'],
       col4: ['A', 'B', 'C', 'D']
     };
+    this.input = 'Hello World';
   }
 
   setEncryptText(value) {
@@ -18,9 +21,25 @@ class AES extends Component {
     });
   }
 
+  encrypt(input) {
+    var blocks = [];
+    var temp = true;
+    while (temp) {
+      var matrix = [];
+      var i = 0;
+      while (sizeof(matrix) < 16) {
+        matrix.push(input.charCodeAt(i));
+        i++;
+      }
+      blocks.push(matrix);
+      temp = false;
+    }
+  }
+
   render() {
     return (
       <div className="AES">
+        <button onClick={this.encrypt(this.input)}>Test</button>
         <AES_MATRIX value={this.state}></AES_MATRIX>
       </div>
     );
